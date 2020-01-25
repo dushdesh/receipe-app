a4bdb5c34e9ae03ee43a0db9b52893d1e4731ad9
Dushyant Deshmukh <dushyant.deshmukh@hp.com>
2020-01-24 21:57:11 -0500

    Wiring it up

---
+++ src/test/java/com/dushdesh/receipeapp/ReceipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dushdesh.receipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
